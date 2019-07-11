8e76d56fcaa9134cbce80c7b703aba57fdf77aa2
shubhamkr96 <kmrshubham47@gmail.com>
2019-07-11 15:44:15 +0530

    Updating Test Cases

--- Assignment1/test_word_report.py
+++ Assignment1/test_word_report.py
@@ -8,14 +8,14 @@ class Test_word_count(TestCase):
         self.wc = WordCount()
 
     def test_word_count(self):
-        self.assertEqual(0, len(self.wc.word_count("")))
-        self.assertEqual({}, self.wc.word_count(""))
-        self.assertEqual({}, self.wc.word_count("?!@#$%^&*({}|?><+=- "))
-        self.assertEqual({'this':1, 'is':1, 'a':1, 'test':1}, self.wc.word_count("This is a test."))
-        self.assertEqual(5, len(self.wc.word_count("This is a test.\n This is a another test.")))
+        self.assertEqual(len(self.wc.word_count("")), 0)
+        self.assertEqual(self.wc.word_count(""), {})
+        self.assertEqual(self.wc.word_count("?!@#$%^&*({}|?><+=- /*"), {})
+        self.assertEqual(self.wc.word_count("This is a test."), {'this':1, 'is':1, 'a':1, 'test':1})
+        self.assertEqual(len(self.wc.word_count("This is a test.\n This is a another test.")), 5)
 
     def test_word_report(self):
-        pass
+        pass # 4476
 
     def test_generate_report(self):
         pass
