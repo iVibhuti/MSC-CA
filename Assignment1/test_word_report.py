ef0350a44fd58517e30808c3e4f9821fc9e4f2a6
shubhamkr96 <kmrshubham47@gmail.com>
2019-07-11 15:33:22 +0530

    Updating Test Cases

--- Assignment1/test_word_report.py
+++ Assignment1/test_word_report.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
-from .word_count import word_report, generate_report
+from .word_count import WordCount
 
-class TestWord_report(TestCase):
+
+class Test_word_count(TestCase):
+
+    def setUp(self):
+        self.wc = WordCount()
+
+    def test_word_count(self):
+        self.assertEqual(0, len(self.wc.word_count("")))
+        self.assertEqual({}, self.wc.word_count(""))
+        self.assertEqual({}, self.wc.word_count("?!@#$%^&*({}|?><+=- "))
+        self.assertEqual({'this':1, 'is':1, 'a':1, 'test':1}, self.wc.word_count("This is a test."))
+        self.assertEqual(5, len(self.wc.word_count("This is a test.\n This is a another test.")))
+
     def test_word_report(self):
-        self.fail()
+        pass
+
+    def test_generate_report(self):
+        pass
+
+    def tearDown(self):
+        del self.wc
